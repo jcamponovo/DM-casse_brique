4081244f90128a9784b03dd6a6d57ad3b1d504bd
Francesco <117632580+francescoboneschi@users.noreply.github.com>
2023-01-09 08:39:27 +0100

    Update code pixel 1.py

--- code pixel 1.py
+++ code pixel 1.py
@@ -1,90 +1,74 @@
 import pyxel
 
-pyxel.init(128, 128, title="Nuit du c0de")
+# Largeur et hauteur de la fenêtre de jeu
+WIDTH = 240
+HEIGHT = 160
 
-vaisseau_x = 60
-vaisseau_y = 124
+# Constantes pour la taille de la balle et de la raquette
+BALL_SIZE = 4
+PADDLE_WIDTH = 48
+PADDLE_HEIGHT = 8
 
-# initialisation des tirs
-tirs_liste = []
+# Constantes pour la vitesse de la balle et de la raquette
+BALL_SPEED = 2
+PADDLE_SPEED = 4
 
+# Variables pour la position de la balle et de la raquette
+ball_x = 0
+ball_y = 0
+paddle_x = 0
+paddle_y = 150
 
-def vaisseau_deplacement(x, y):
-    """déplacement avec les touches de directions"""
+# Variable pour la direction de la balle (1 = vers la droite, -1 = vers la gauche)
+ball_dir = 1
 
-    if pyxel.btn(pyxel.KEY_RIGHT):
-        if (x < 120) :
-            x = x + 2
-    if pyxel.btn(pyxel.KEY_LEFT):
-        if (x > 0) :
-            x = x - 2
-    return x, y
-def update():
-    """mise à jour des variables (30 fois par seconde)"""
-
-    global vaisseau_x, vaisseau_y
-
-    vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
-def draw():
-    """création des objets (30 fois par seconde)"""
+# Initialise le jeu
+def init():
+    # Initialise l'écran de jeu
+    pyxel.init(WIDTH, HEIGHT)
     
-    pyxel.cls(0)
+    # Place la balle et la raquette au milieu de l'écran
+    global ball_x, ball_y, paddle_x
+    ball_x = WIDTH // 2
+    ball_y = HEIGHT // 2
+    paddle_x = WIDTH // 2 - PADDLE_WIDTH // 2
 
-    pyxel.rect(vaisseau_x, vaisseau_y, 25, 10, 1)
-
-def tirs_creation(x, y, tirs_liste):
-    """création d'un tir avec la barre d'espace"""
-
-    # btnr pour eviter les tirs multiples
-    if pyxel.btnr(pyxel.KEY_SPACE):
-        tirs_liste.append([x+4, y-4])
-    return tirs_liste
-
-def tirs_deplacement(tirs_liste):
-    """déplacement des tirs vers le haut et suppression s'ils sortent du cadre"""
-
-    for tir in tirs_liste:
-        tir[1] -= 0.8
-        if  tir[1]<-8:
-            tirs_liste.remove(tir)
-    return tirs_liste
-
-
-# =========================================================
-# == UPDATE
-# =========================================================
+# Met à jour l'état du jeu
 def update():
-    """mise à jour des variables (30 fois par seconde)"""
-
-    global vaisseau_x, vaisseau_y, tirs_liste
-
-    # mise à jour de la position du vaisseau
-    vaisseau_x, vaisseau_y = vaisseau_deplacement(vaisseau_x, vaisseau_y)
-
-    # creation des tirs en fonction de la position du vaisseau
-    tirs_liste = tirs_creation(vaisseau_x, vaisseau_y, tirs_liste)
-
-    # mise a jour des positions des tirs
-    tirs_liste = tirs_deplacement(tirs_liste)
-
-
-# =========================================================
-# == DRAW
-# =========================================================
-def draw():
-    """création des objets (30 fois par seconde)"""
-
-    # vide la fenetre
-    pyxel.cls(0)
-
-    # vaisseau (carre 8x8)
-    pyxel.rect(vaisseau_x, vaisseau_y, 25, 10, 1)
-
-    # tirs
-    for tir in tirs_liste:
-        pyxel.rect(tir[0], tir[1], 5, 3, 6)    
+    global ball_x, ball_y, paddle_x, ball_dir
     
-pyxel.run(update, draw)
+    # Déplace la raquette si les flèches gauche et droite sont enfoncées
+    if pyxel.btn(pyxel.KEY_LEFT) and paddle_x > 0:
+        paddle_x -= PADDLE_SPEED
+    if pyxel.btn(pyxel.KEY_RIGHT) and paddle_x < WIDTH - PADDLE_WIDTH:
+        paddle_x += PADDLE_SPEED
+    
+    # Déplace la balle
+    ball_x += BALL_SPEED * ball_dir
+    ball_y += BALL_SPEED
+    
+    # Inverse la direction de la balle si elle touche un bord
+    if ball_x <= 0 or ball_x >= WIDTH - BALL_SIZE:
+        ball_dir = -ball_dir
+    if ball_y <= 0:
+        ball_y = 0
+    
+    # Si la balle touche la raquette, renvoie la balle dans l'autre sens
+    if (
+        ball_x >= paddle_x and ball_x <= paddle_x + PADDLE_WIDTH
+        and ball_y + BALL_SIZE >= paddle_y
+    ):
+        ball_y = paddle_y - BALL_SIZE
+        ball_dir = (ball_x - paddle_x) // PADDLE_WIDTH - 1
+
+# Dessine l'écran de jeu
+def draw():
+    # Efface l'écran
+    pyxel.cls(0)
+    
+    # Dessine la balle et la raquette
+    pyx el.rect(paddle_x, paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT, 9)
+    pyxel.rect(ball_x, ball_y, BALL_SIZE, BALL_SIZE, 11)
 
  
 
